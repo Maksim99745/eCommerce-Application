2723dcf5875b19323609529d84f20966fe551e28
Maksim99745 <maksimmuhametzyanov11@gmail.com>
2024-05-02 16:38:15 +0300

    feat: add Jest to the project

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
+//demo tests for test Jest framework
+test('demo', () => {
+  expect(true).toBe(true);
+});
+
+test('Render the main page', () => {
+  render(<App />);
+  expect(true).toBeTruthy();
+});
