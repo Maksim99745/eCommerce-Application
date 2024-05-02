dc5ef29bd2306e49832150b5822da5509f413a80
Maksim99745 <maksimmuhametzyanov11@gmail.com>
2024-05-02 20:13:36 +0300

    fix/RSS-ECOMM-1_13: rename mocks, add test to pre-push

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,6 @@ export default {
     // process `*.tsx` files with `ts-jest`
   },
   moduleNameMapper: {
-    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/mocks/mocks.js',
   },
 };
