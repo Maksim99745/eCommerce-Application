0af5ff3b388ef2acdedee443bc4e15e49028bfb0
Roman Gromov <gromov.r.v.92@gmail.com>
2024-05-06 15:17:51 +0500

    feat/RSS-ECOMM-1_07: Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: { global: 'globalThis' },
   plugins: [react()],
   envPrefix: 'CTP_',
   resolve: {
