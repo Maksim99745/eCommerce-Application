f8f0965dd323f04c61d3e664dcd6fff701cadb78
Roman Gromov <gromov.r.v.92@gmail.com>
2024-05-06 20:27:41 +0500

    fix/RSS-ECOMM-1_24: Vite build (#50)

--- README.md
+++ README.md
@@ -33,11 +33,10 @@ Project goals:
 - Linters: [**ESLint**](https://eslint.org/), [**Prettier**](https://prettier.io/)
 - Pre-push/Pre-commit: [**Husky**](https://typicode.github.io/husky/)
 - UI-KIT: [**Material UI**](https://mui.com/material-ui/)
-- API: [**axios**](https://axios-http.com/docs/intro)
+- API: [**@commercetools/sdk-client-v2, @commercetools/platform-sdk**](https://docs.commercetools.com/sdk/javascript-sdk)
 - Tests: [**Jest**](https://jestjs.io/)
 - Routing: [**React Router**](https://reactrouter.com/en/main) [**Routes map**]()
 - Hosting: [**Netlify**](https://www.netlify.com/)
-- Backend: [**commerce-tools**](https://commercetools.com/)
 - Task Board: [**GitHub Pages**](https://github.com/users/Maksim99745/projects/2/views/1)
 - Code style: [**Wiki**]()
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,14 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'node:path';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills({
-      include: ['stream', 'http', 'https', 'zlib'],
-    }),
-  ],
+  plugins: [react()],
   envPrefix: 'CTP_',
   resolve: {
     alias: {
-      stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+      'node-fetch': 'isomorphic-fetch',
       '@core': resolve(__dirname, './src/core/'),
       '@pages': resolve(__dirname, './src/pages/'),
       '@components': resolve(__dirname, './src/components/'),
@@ -22,18 +16,6 @@ export default defineConfig({
       '@enums': resolve(__dirname, './src/enums/'),
       '@constants': resolve(__dirname, './src/constants/'),
       '@utils': resolve(__dirname, './src/utils/'),
-    },
-  },
-  build: {
-    chunkSizeWarningLimit: 600,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-      },
     },
   },
 });
