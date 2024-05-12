29579290b78a8e26a3bb4e002a11f52e82965504
tatiana <belangelphone@gmail.com>
2024-05-12 20:09:05 +0300

    update/RSS-ECOMM-2_01: improve zod validation

--- src/core/validation/user-login.validation.schema.ts
+++ src/core/validation/user-login.validation.schema.ts
@@ -2,18 +2,27 @@ import { z } from 'zod';
 
 const MIN_LENGTH = 8;
 const MAX_LENGTH = 20;
+const EMAIL_MAX_LENGTH = 40;
 const TEST_PASSWORD_REG_EX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,20}$/;
 
 export const loginFormSchema = z.object({
-  email: z.string({ required_error: 'Email is required' }).email('Invalid email'),
+  email: z
+    .string({ required_error: 'Email is required' })
+    .max(EMAIL_MAX_LENGTH, `Email should not be longer, than ${EMAIL_MAX_LENGTH} characters`)
+    .regex(/.+/, 'Email is required')
+    .regex(/@/, 'Email should contain symbol "@"')
+    .regex(/^[^@]+@[^@]+\.[^@]+$/, 'Email should contain domainName')
+    .regex(/^[^\s]+$/, 'Email should not contain whitespace')
+    .email('Email address should be properly formatted (e.g., user@example.com)'),
   password: z
     .string()
-    .min(MIN_LENGTH, `Password must be at least ${MIN_LENGTH} characters long`)
-    .max(MAX_LENGTH, `Password must not be more, than ${MAX_LENGTH} characters`)
-    .regex(/^[^\s]+$/, 'Password must not contain whitespace')
-    .regex(/(?=.*[A-Z])/, 'Password must contain at least 1 uppercase letter')
-    .regex(/(?=.*[a-z])/, 'Password must contain at least 1 lowercase letter')
-    .regex(/(?=.*\d)/, 'Password must contain at least one digit (0-9)')
-    .regex(/(?=.*[!@#$%^&])/, 'Password must contain at least one special character (!@#$%^&)')
+    .regex(/.+/, 'Password is required')
+    .regex(/^[^\s]+$/, 'Password should not contain whitespace')
+    .regex(/(?=.*[A-Z])/, 'Password should contain at least 1 uppercase letter')
+    .regex(/(?=.*[a-z])/, 'Password should contain at least 1 lowercase letter')
+    .regex(/(?=.*\d)/, 'Password should contain at least one digit (0-9)')
+    .regex(/(?=.*[!@#$%^&])/, 'Password should contain at least one special character (!@#$%^&)')
+    .min(MIN_LENGTH, `Password should be at least ${MIN_LENGTH} characters long`)
+    .max(MAX_LENGTH, `Password should not be more, than ${MAX_LENGTH} characters`)
     .regex(TEST_PASSWORD_REG_EX, 'Invalid password'),
 });
