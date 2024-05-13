222a6dbe8971918cc141089620d53b09ab12fa2d
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2024-05-13 12:35:44 +0300

    Feat/RSS-ECOMM-2_01: Implement client-side validation for login form (#73)
    * feat/RSS-ECOMM-2_01: describe tests
    
    * feat/RSS-ECOMM-2_01: add passwordValidationRegEx
    
    * refactor/RSS-ECOMM-2_01: edit schema
    
    * refactor/RSS-ECOMM-2_01: rename variable name
    
    Co-authored-by: JsPowWow <100322298+JsPowWow@users.noreply.github.com>
    
    * refactor/RSS-ECOMM-2_01-login-form: edit test name
    
    Co-authored-by: JsPowWow <100322298+JsPowWow@users.noreply.github.com>
    
    * refactor/RSS-ECOMM-2_01-login-form: edit test name
    
    Co-authored-by: JsPowWow <100322298+JsPowWow@users.noreply.github.com>
    
    * Update src/pages/Login/Login.test.tsx
    
    Co-authored-by: JsPowWow <100322298+JsPowWow@users.noreply.github.com>
    
    * Update src/pages/Login/Login.test.tsx
    
    Co-authored-by: JsPowWow <100322298+JsPowWow@users.noreply.github.com>
    
    * Update src/pages/Login/Login.test.tsx
    
    Co-authored-by: JsPowWow <100322298+JsPowWow@users.noreply.github.com>
    
    * Update src/pages/Login/Login.test.tsx
    
    Co-authored-by: JsPowWow <100322298+JsPowWow@users.noreply.github.com>
    
    * Update src/pages/Login/schema.ts
    
    Co-authored-by: JsPowWow <100322298+JsPowWow@users.noreply.github.com>
    
    * Update src/pages/Login/schema.ts
    
    Co-authored-by: JsPowWow <100322298+JsPowWow@users.noreply.github.com>
    
    * Update src/pages/Login/schema.ts
    
    Co-authored-by: JsPowWow <100322298+JsPowWow@users.noreply.github.com>
    
    * Update src/pages/Login/schema.ts
    
    Co-authored-by: JsPowWow <100322298+JsPowWow@users.noreply.github.com>
    
    * Update src/pages/Login/Login.test.tsx
    
    Co-authored-by: JsPowWow <100322298+JsPowWow@users.noreply.github.com>
    
    * Update src/pages/Login/Login.test.tsx
    
    Co-authored-by: JsPowWow <100322298+JsPowWow@users.noreply.github.com>
    
    * Update src/pages/Login/Login.test.tsx
    
    Co-authored-by: JsPowWow <100322298+JsPowWow@users.noreply.github.com>
    
    * Update src/pages/Login/Login.test.tsx
    
    Co-authored-by: JsPowWow <100322298+JsPowWow@users.noreply.github.com>
    
    * Update src/pages/Login/Login.test.tsx
    
    Co-authored-by: JsPowWow <100322298+JsPowWow@users.noreply.github.com>
    
    * Update src/pages/Login/Login.test.tsx
    
    Co-authored-by: JsPowWow <100322298+JsPowWow@users.noreply.github.com>
    
    * Update src/pages/Login/Login.test.tsx
    
    Co-authored-by: JsPowWow <100322298+JsPowWow@users.noreply.github.com>
    
    * refactor/RSS-ECOMM-2_01: rename test name
    
    * update/RSS-ECOMM-1_02: Implement tests for login page
    
    * update/RSS-ECOMM-1_02: Implement tests for login page
    
    * update/RSS-ECOMM-1_02: Implement login form
    
    * update/RSS-ECOMM-2_01: update according to mentor suggestions
    
    * refactor/RSS-ECOMM-2_01: replace "must" with "should"
    
    * update/RSS-ECOMM-2_01: improve zod validation
    
    * update/RSS-ECOMM-2_01: edit tests
    
    * refactor/RSS-ECOMM-2_01: replace validation tests to a separate file
    
    * update/RSS-ECOMM-2_01: add mode onChange to validate while typing
    
    * update/RSS-ECOMM-2_01: add mode onChange to validate while typing
    
    ---------
    
    Co-authored-by: JsPowWow <100322298+JsPowWow@users.noreply.github.com>

---
+++ src/core/validation/user-login.validation.schema.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+
+const MIN_LENGTH = 8;
+const MAX_LENGTH = 20;
+const TEST_PASSWORD_REG_EX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,20}$/;
+
+export const loginFormSchema = z.object({
+  email: z
+    .string({ required_error: 'Email is required' })
+    .regex(/.+/, 'Email is required')
+    .regex(/(?=.*@)/, 'Email should contain symbol "@"')
+    .regex(/^[^@]+@[^@]+\.[^@]+$/, 'Email should contain domain name')
+    .regex(/^[^\s]+$/, 'Email should not contain whitespace')
+    .email('Email address should be properly formatted (e.g., user@example.com)'),
+  password: z
+    .string()
+    .regex(/.+/, 'Password is required')
+    .regex(/^[^\s]+$/, 'Password should not contain whitespace')
+    .regex(/(?=.*[A-Z])/, 'Password should contain at least 1 uppercase letter')
+    .regex(/(?=.*[a-z])/, 'Password should contain at least 1 lowercase letter')
+    .regex(/(?=.*\d)/, 'Password should contain at least one digit (0-9)')
+    .regex(/(?=.*[!@#$%^&])/, 'Password should contain at least one special character (!@#$%^&)')
+    .min(MIN_LENGTH, `Password should be at least ${MIN_LENGTH} characters long`)
+    .max(MAX_LENGTH, `Password should not be more, than ${MAX_LENGTH} characters`)
+    .regex(TEST_PASSWORD_REG_EX, 'Invalid password'),
+});
--- src/pages/Login/Login.page.tsx
+++ src/pages/Login/Login.page.tsx
@@ -1,5 +1,22 @@
-function LoginPage() {
-  return <p>Login page</p>;
+import { ReactNode, useCallback } from 'react';
+import { Box } from '@mui/material';
+import { LoginForm, LoginFormProps } from './components/LoginForm';
+
+const pageStyles = {
+  loginPage: { display: 'flex', margin: '0 auto' },
+};
+
+function LoginPage(): ReactNode {
+  const handleFormSubmit = useCallback<LoginFormProps['onSubmit']>(async (data) => {
+    console.log(data);
+    return Promise.resolve('Ok');
+  }, []);
+
+  return (
+    <Box sx={pageStyles.loginPage}>
+      <LoginForm onSubmit={handleFormSubmit} />
+    </Box>
+  );
 }
 
 export default LoginPage;
