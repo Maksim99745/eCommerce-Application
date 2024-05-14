3b0409d74ce189eefff28feda2c374d75aef0ef2
Maksim Mukhametzianov <137721533+Maksim99745@users.noreply.github.com>
2024-05-14 12:37:25 +0200

    feat/RSS-ECOMM-2_09: Implement client-side validation (#77)
    * feat/RSS-ECOMM-2_09: add 4 first fields
    
    * fix/RSS-ECOMM-2_24: working under date picker
    
    * feat/RSS-ECOMM-2_09: add address form
    
    * fix/RSS-ECOMM-2_24: working under country post code
    
    * feat/RSS-ECOMM-2_09: registration form progress
    
    * fix/RSS-ECOMM-2_09: schema progress
    
    * fix/RSS-ECOMM-2_09: schema progress
    
    * fix/RSS-ECOMM-2_09: fix issues
    
    * feat/RSS-ECOMM-2_09: connect login and registration validations
    
    * feat/RSS-ECOMM-2_09: add first tests
    
    * feat/RSS-ECOMM-2_09: add basic tests
    
    * feat/RSS-ECOMM-2_09: add basic tests

---
+++ src/core/validation/user-registration.validation.test.ts
@@ -0,0 +1,46 @@
+import '@testing-library/jest-dom';
+import { registrationSchema } from './user-registration.validation.schema';
+
+test('first name should be provided', () => {
+  const result = registrationSchema.safeParse({});
+  expect(result.success).toBe(false);
+  expect(result.error?.issues[0].message).toBe('First name is required');
+});
+
+test('last name should be provided', () => {
+  const result = registrationSchema.safeParse({});
+  expect(result.success).toBe(false);
+  expect(result.error?.issues[1].message).toBe('Last name is required');
+});
+
+// TODO: divide date check for empty and older
+
+test('user birth date should be provided', () => {
+  const result = registrationSchema.safeParse({});
+  expect(result.success).toBe(false);
+  expect(result.error?.issues[2].message).toBe('User should be older than 13 y.o.');
+});
+
+test('street should be provided', () => {
+  const result = registrationSchema.safeParse({});
+  expect(result.success).toBe(false);
+  expect(result.error?.issues[3].message).toBe('Street is required');
+});
+
+test('city should be provided', () => {
+  const result = registrationSchema.safeParse({});
+  expect(result.success).toBe(false);
+  expect(result.error?.issues[4].message).toBe('City is required');
+});
+
+test('country should be provided', () => {
+  const result = registrationSchema.safeParse({});
+  expect(result.success).toBe(false);
+  expect(result.error?.issues[5].message).toBe('Please select country');
+});
+
+test('postal code should be provided', () => {
+  const result = registrationSchema.safeParse({});
+  expect(result.success).toBe(false);
+  expect(result.error?.issues[6].message).toBe('Please provide postal code');
+});
---
+++ src/pages/Registration/components/RegistrationForm.tsx
@@ -0,0 +1,83 @@
+import {
+  FormContainer,
+  TextFieldElement,
+  PasswordElement,
+  SelectElement,
+  DatePickerElement,
+} from 'react-hook-form-mui';
+
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { UserRegistrationData } from '@models/index';
+import { Box, Button, Typography } from '@mui/material';
+import { countriesOptions, defaultCountryOption } from '@constants/countries.const';
+import { registrationSchema } from '@core/validation/user-registration.validation.schema';
+
+const formStyles = {
+  form: { display: 'flex', flexDirection: 'column', width: '100%', height: '100%' },
+  submitButton: { width: 100 },
+  textField: { minWidth: 410 },
+  divider: { height: '1px', backgroundColor: 'black' },
+};
+
+export interface RegistrationFormProps {
+  onSubmit: (registrationData: UserRegistrationData) => void;
+}
+
+export function RegistrationForm({ onSubmit }: RegistrationFormProps) {
+  return (
+    <FormContainer<UserRegistrationData>
+      resolver={zodResolver(registrationSchema)}
+      defaultValues={{
+        email: '',
+        password: '',
+        firstName: '',
+        lastName: '',
+        birthDate: '',
+        street: '',
+        city: '',
+        country: defaultCountryOption.id,
+        postalCode: '',
+      }}
+      onSuccess={onSubmit}
+      mode="onChange"
+    >
+      <Box sx={formStyles.form}>
+        <Typography variant="h4" gutterBottom>
+          Sign in
+        </Typography>
+        <TextFieldElement<UserRegistrationData> name="email" label="Email" required helperText=" " />
+        <PasswordElement<UserRegistrationData>
+          margin="dense"
+          label="Password"
+          required
+          name="password"
+          helperText=" "
+        />
+        <TextFieldElement<UserRegistrationData> name="firstName" label="First Name" required helperText=" " />
+        <TextFieldElement<UserRegistrationData> name="lastName" label="Last Name" required helperText=" " />
+
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePickerElement<UserRegistrationData> name="birthDate" label="Birth Date" required helperText=" " />
+        </LocalizationProvider>
+        <Typography variant="h6" gutterBottom>
+          Pass your address
+        </Typography>
+        <SelectElement<UserRegistrationData>
+          label="Country"
+          name="country"
+          options={countriesOptions}
+          helperText=" "
+          required
+        />
+        <TextFieldElement<UserRegistrationData> name="postalCode" label="Postal Code" required helperText=" " />
+        <TextFieldElement<UserRegistrationData> name="city" label="City" required helperText=" " />
+        <TextFieldElement<UserRegistrationData> name="street" label="Street" required helperText=" " />
+        <Button type="submit" variant="contained" color="primary" sx={formStyles.submitButton}>
+          Submit
+        </Button>
+      </Box>
+    </FormContainer>
+  );
+}
