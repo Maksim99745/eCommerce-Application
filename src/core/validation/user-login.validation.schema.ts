4578f7c4bbecb938bef92f332774f5b63d013298
tatiana <belangelphone@gmail.com>
2024-05-15 12:15:43 +0300

    refactor/RSS-ECOMM-2_06: edit validation

--- src/core/validation/user-login.validation.schema.ts
+++ src/core/validation/user-login.validation.schema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 const MIN_LENGTH = 8;
 const MAX_LENGTH = 20;
-const TEST_PASSWORD_REG_EX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,20}$/;
+const TEST_PASSWORD_REG_EX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*.])[A-Za-z\d!@#$%^&*.]{8,20}$/;
 
 export const loginFormSchema = z.object({
   email: z
@@ -20,9 +20,9 @@ export const loginFormSchema = z.object({
     .regex(/(?=.*[A-Z])/, 'Password should contain at least 1 English uppercase letter')
     .regex(/(?=.*[a-z])/, 'Password should contain at least 1 English lowercase letter')
     .regex(/(?=.*\d)/, 'Password should contain at least one digit (0-9)')
-    .regex(/(?=.*[!@#$%^&])/, 'Password should contain at least one special symbol (!@#$%^&)')
+    .regex(/(?=.*[!@#$%^&*.])/, 'Password should contain at least one special symbol (!@#$%^&*.)')
     .min(MIN_LENGTH, `Password length should be at least ${MIN_LENGTH} characters long`)
     .max(MAX_LENGTH, `Password should not be more, than ${MAX_LENGTH} characters`)
-    .regex(/^[a-zA-Z0-9!@#$%^&*]+$/, 'Password should contain only English letters, numbers and special symbols')
+    .regex(/^[a-zA-Z0-9!@#$%^.&*]+$/, 'Password may contain only English letters, numbers & symbols (!@#$%^&*.)')
     .regex(TEST_PASSWORD_REG_EX, 'Invalid password'),
 });
