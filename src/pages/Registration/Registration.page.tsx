778bb3304f271d0fad6f1dd3b240031e5fd2e500
Maksim99745 <maksimmuhametzyanov11@gmail.com>
2024-05-15 12:39:23 +0300

    fix/RSS-ECOMM-2_16: delete unneeded styles-2

--- src/pages/Registration/Registration.page.tsx
+++ src/pages/Registration/Registration.page.tsx
@@ -3,17 +3,13 @@ import { Stack, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { RegistrationForm, RegistrationFormProps } from './components/RegistrationForm';
 
-const pageStyles = {
-  registrationPage: { display: 'flex', margin: '0 auto' },
-};
-
 function RegistrationPage(): ReactNode {
   const handleFormSubmit = useCallback<RegistrationFormProps['onSubmit']>(async (data) => {
     console.log(data);
     return Promise.resolve('Ok');
   }, []);
   return (
-    <Stack sx={pageStyles.registrationPage}>
+    <Stack>
       <RegistrationForm onSubmit={handleFormSubmit} />
       <Typography marginTop={2}>
         Already have an account? <Link to="/login">Login in</Link>
