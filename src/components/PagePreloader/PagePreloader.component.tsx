d4e89ff44541d5500e881bc6c85fc402d6178748
Maksim99745 <maksimmuhametzyanov11@gmail.com>
2024-05-17 13:39:40 +0300

    feat/RSS-ECOMM-2_25: delete unneeded styles

--- src/components/PagePreloader/PagePreloader.component.tsx
+++ src/components/PagePreloader/PagePreloader.component.tsx
@@ -3,14 +3,13 @@ import { Skeleton, Stack } from '@mui/material';
 export function PagePreloader() {
   return (
     <Stack spacing={{ xs: 2.5 }}>
-      <Skeleton variant="rectangular" width={'100%'} height={60} />
-      <Skeleton variant="rectangular" width={'100%'} height={60} />
-      <Skeleton variant="rectangular" width={'100%'} height={60} />
-      <Skeleton variant="rectangular" width={'100%'} height={60} />
-      <Skeleton variant="rectangular" width={'100%'} height={60} />
-      <Skeleton variant="rectangular" width={'100%'} height={60} />
-      <Skeleton variant="rectangular" width={'100%'} height={60} />
+      <Skeleton variant="rectangular" width="100%" height={60} />
+      <Skeleton variant="rectangular" width="100%" height={60} />
+      <Skeleton variant="rectangular" width="100%" height={60} />
+      <Skeleton variant="rectangular" width="100%" height={60} />
+      <Skeleton variant="rectangular" width="100%" height={60} />
+      <Skeleton variant="rectangular" width="100%" height={60} />
+      <Skeleton variant="rectangular" width="100%" height={60} />
     </Stack>
   );
 }
-// return <CircularProgress sx={{ alignSelf: 'center', marginTop: '50%' }} />;
