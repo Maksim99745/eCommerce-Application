d7f4aefcef159db56b41afeefe1e8c9a189c85c4
Maksim99745 <maksimmuhametzyanov11@gmail.com>
2024-05-21 23:17:26 +0300

    feat/RSS-ECOMM-3_14: add basic data

--- src/pages/Profile/Profile.page.test.tsx
+++ src/pages/Profile/Profile.page.test.tsx
@@ -1,5 +1,23 @@
 import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { act } from 'react';
+import { DEFAULT_REQUEST_DELAY } from '@test/constants/time.const';
+import ProfilePage from './Profile.page';
+
+jest.mock('@core/api/api.service', () => ({
+  apiService: {
+    getCustomer: jest.fn(
+      () =>
+        new Promise((resolve) => {
+          setTimeout(() => resolve(null), DEFAULT_REQUEST_DELAY);
+        }),
+    ),
+  },
+}));
 
 test('Render the profile page', async () => {
-  expect(true).toBeTruthy();
+  await act(async () => {
+    render(<ProfilePage />);
+    expect(true).toBeTruthy();
+  });
 });
--- src/pages/Profile/Profile.page.tsx
+++ src/pages/Profile/Profile.page.tsx
@@ -2,19 +2,16 @@ import { apiService } from '@core/api/api.service';
 import { PagePreloader } from '@components/PagePreloader/PagePreloader.component';
 import { Stack } from '@mui/material';
 import { useRequest } from '@core/api/use-request.hook';
-import { useShowMessage } from '@hooks/useShowMessage';
 import PersonalData from './components/PersonalData.component';
 
 function ProfilePage() {
   const { data, isLoading } = useRequest('customer', () => apiService.getCustomer());
-  const showMessage = useShowMessage();
 
   if (isLoading) {
     return <PagePreloader />;
   }
 
   if (!data) {
-    showMessage('User personal information not found', 'error');
     return null;
   }
 
