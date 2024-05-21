3f70f401114b26545b9023d265b2d0105d110afd
Maksim Mukhametzianov <137721533+Maksim99745@users.noreply.github.com>
2024-05-21 15:04:50 +0200

    fix/RSS-ECOMM-3_23: fix address styles (#119)
    * fix/RSS-ECOMM-3_23: fix address select styles
    
    * fix/RSS-ECOMM-3_23: fix address select styles
    
    * fix/RSS-ECOMM-3_23: fix issues

--- src/components/PagePreloader/PagePreloader.component.tsx
+++ src/components/PagePreloader/PagePreloader.component.tsx
@@ -1,6 +1,20 @@
 import { Skeleton, Stack } from '@mui/material';
+import { useEffect, useState } from 'react';
+
+const SKELETON_DISPLAY_DELAY = 200;
 
 export function PagePreloader() {
+  const [empty, setEmpty] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setEmpty(false);
+    }, SKELETON_DISPLAY_DELAY);
+  }, []);
+
+  if (empty) {
+    return null;
+  }
   return (
     <Stack spacing={{ xs: 2.5 }}>
       <Skeleton variant="rectangular" width="100%" height={60} />
