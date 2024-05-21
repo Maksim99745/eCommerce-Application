1d6f0e49d8839b744d298ee4cd1c00678f3dffb0
Maksim99745 <maksimmuhametzyanov11@gmail.com>
2024-05-21 18:47:02 +0300

    feat/RSS-ECOMM-3_14: add basic data

--- src/pages/Profile/Profile.page.tsx
+++ src/pages/Profile/Profile.page.tsx
@@ -1,5 +1,30 @@
+import { apiService } from '@core/api/api.service';
+import { useState } from 'react';
+import { Customer } from '@commercetools/platform-sdk';
+import { PagePreloader } from '@components/PagePreloader/PagePreloader.component';
+import useOnce from '@hooks/useOnce';
+import { Stack } from '@mui/material';
+import PersonalData from './components/PersonalData.component';
+
+let data: Customer = await apiService.getCustomer();
+
 function ProfilePage() {
-  return <p>Profile page</p>;
+  const [customerData, setCustomerData] = useState(data);
+
+  const fetchCustomerData = async () => {
+    data = await apiService.getCustomer();
+    setCustomerData(data);
+  };
+
+  useOnce(fetchCustomerData);
+
+  const { firstName, lastName, dateOfBirth } = customerData;
+
+  return (
+    <Stack>
+      {data ? <PersonalData firstName={firstName} lastName={lastName} dateOfBirth={dateOfBirth} /> : <PagePreloader />}
+    </Stack>
+  );
 }
 
 export default ProfilePage;
---
+++ src/pages/Profile/components/PersonalData.component.tsx
@@ -0,0 +1,30 @@
+import { Paper, Stack, Typography } from '@mui/material';
+
+export interface PersonalDataProps {
+  firstName?: string;
+  lastName?: string;
+  dateOfBirth?: string;
+}
+
+function PersonalData({ firstName = '', lastName = '', dateOfBirth = '' }: PersonalDataProps) {
+  return (
+    <Paper elevation={1} sx={{ p: '1vh 2%', maxWidth: '900px', width: '100%' }}>
+      <Stack>
+        <Typography variant="h5" gutterBottom>
+          Personal information
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          First name: {firstName}
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Last name: {lastName}
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Birth date: {dateOfBirth}
+        </Typography>
+      </Stack>
+    </Paper>
+  );
+}
+
+export default PersonalData;
