05567d5b640e5cdbf642d9129afd75e45651f6ea
Maksim99745 <maksimmuhametzyanov11@gmail.com>
2024-05-21 21:38:44 +0300

    feat/RSS-ECOMM-3_14: add basic data

--- src/pages/Profile/Profile.page.test.tsx
+++ src/pages/Profile/Profile.page.test.tsx
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import ProfilePage from './Profile.page';
 
-test('Render the profile page', () => {
+test('Render the profile page', async () => {
   render(<ProfilePage />);
   expect(true).toBeTruthy();
 });
