ecbb4f7cf9680a37802e9d1b5e71c1af670214cd
Maksim99745 <maksimmuhametzyanov11@gmail.com>
2024-05-22 12:27:23 +0300

    fix/RSS-ECOMM-3_14: solve conflicts

--- src/pages/Profile/Profile.page.test.tsx
+++ src/pages/Profile/Profile.page.test.tsx
@@ -1,9 +1,20 @@
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
+import { act } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ProfilePage from './Profile.page';
 
+jest.mock('@hooks/useAuth', () => () => ({ user: jest.fn() }));
+
 // eslint-disable-next-line jest/no-disabled-tests
-test.skip('Render the profile page', () => {
-  render(<ProfilePage />);
-  expect(true).toBeTruthy();
+test.skip('Render the profile page', async () => {
+  await act(async () => {
+    render(<ProfilePage />);
+    expect(true).toBeTruthy();
+    render(
+      <BrowserRouter>
+        <ProfilePage />
+      </BrowserRouter>,
+    );
+  });
 });
