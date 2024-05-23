cce797158dab565719c05dbdc6c20f48737a9476
Maksim99745 <maksimmuhametzyanov11@gmail.com>
2024-05-23 17:01:36 +0300

    fix/RSS-ECOMM-3_14: fix issues after code-review

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,6 @@
 export { type SelectOption } from './select-option.model';
 export {
   type RegistrationForm,
-  type RegistrationFormAddress,
   type LoginForm,
   type AddressInformationForm,
   type PersonalInformationForm,
