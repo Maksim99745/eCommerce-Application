c76f2533292363080afb6ee311d819ca9de5db30
tatiana <belangelphone@gmail.com>
2024-06-03 17:39:41 +0300

    refactor/RSS-ECOMM-3_21: remove dublications

--- src/pages/Catalog/Catalog.page.tsx
+++ src/pages/Catalog/Catalog.page.tsx
@@ -10,78 +10,35 @@ function CatalogPage() {
     return <CatalogPageSkeleton />;
   }
 
-  if (!category) {
-    return (
-      (
-        <Container maxWidth="xl" sx={{ p: 0, height: '100%' }}>
-          <Stack gap={2} sx={{ height: '100%', overflow: 'auto', scrollbarGutter: 'stable' }}>
-            <Box
-              sx={{
-                width: '100%',
-                minHeight: '200px',
-                position: 'relative',
-                backgroundImage: `url("https://images.prom.ua/5906359888_5906359888.jpg")`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                borderRadius: 3,
-              }}
-            >
-              <Typography
-                variant="h3"
-                sx={{
-                  position: 'absolute',
-                  top: 10,
-                  left: 20,
-                  color: 'white',
-                  fontWeight: 'bold',
-                  textShadow:
-                    '3px 0px 7px rgba(81,67,21,0.8), -3px 0px 7px rgba(81,67,21,0.8), 0px 4px 7px rgba(81,67,21,0.8)',
-                }}
-              >
-                Products Catalog
-              </Typography>
-            </Box>
-            <Paper elevation={1} sx={{ p: 0, width: '100%', flex: 1 }}>
-              <ProductListComponent />
-            </Paper>
-          </Stack>
-        </Container>
-      ) || <Typography variant="h5">Category not found</Typography>
-    );
-  }
-
   return (
     <Container maxWidth="xl" sx={{ p: 0, height: '100%' }}>
       <Stack gap={2} sx={{ height: '100%', overflow: 'auto', scrollbarGutter: 'stable' }}>
-        {category.description?.en && (
-          <Box
+        <Box
+          sx={{
+            width: '100%',
+            minHeight: '200px',
+            position: 'relative',
+            backgroundImage: `url("${category?.description?.en || 'https://images.prom.ua/5906359888_5906359888.jpg'}")`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            borderRadius: 3,
+          }}
+        >
+          <Typography
+            variant="h3"
             sx={{
-              width: '100%',
-              minHeight: '200px',
-              position: 'relative',
-              backgroundImage: `url("${category.description.en}")`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-              borderRadius: 3,
+              position: 'absolute',
+              top: 10,
+              left: 20,
+              color: 'white',
+              fontWeight: 'bold',
+              textShadow:
+                '3px 0px 7px rgba(81,67,21,0.8), -3px 0px 7px rgba(81,67,21,0.8), 0px 4px 7px rgba(81,67,21,0.8)',
             }}
           >
-            <Typography
-              variant="h3"
-              sx={{
-                position: 'absolute',
-                top: 10,
-                left: 20,
-                color: 'white',
-                fontWeight: 'bold',
-                textShadow:
-                  '3px 0px 7px rgba(81,67,21,0.8), -3px 0px 7px rgba(81,67,21,0.8), 0px 4px 7px rgba(81,67,21,0.8)',
-              }}
-            >
-              {category.name.en}
-            </Typography>
-          </Box>
-        )}
-        {!category.description?.en && <Typography variant="h4">{category.name.en} products</Typography>}
+            {category?.name?.en || 'Products Catalog'}
+          </Typography>
+        </Box>
 
         <Paper elevation={1} sx={{ p: 0, width: '100%', flex: 1 }}>
           <ProductListComponent />
