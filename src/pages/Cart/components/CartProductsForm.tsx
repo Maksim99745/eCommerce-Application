569ae2554b82d49f4ca7a9ddcdc600b4e634eada
Maksim99745 <maksimmuhametzyanov11@gmail.com>
2024-06-07 14:35:05 +0300

    feat/RSS-ECOMM-4_10: task progress

--- src/pages/Cart/components/CartProductsForm.tsx
+++ src/pages/Cart/components/CartProductsForm.tsx
@@ -8,7 +8,7 @@ export type CartProductsFormProps = {
 
 export function CartProductsForm({ cartData, isCartLoading = false }: CartProductsFormProps) {
   const { lineItems } = cartData;
-  // console.log(cartData);
+  console.log(cartData);
   return (
     <Stack spacing={0} direction="column" sx={{ overflowX: 'auto' }}>
       {lineItems.map((cartItem) => (
