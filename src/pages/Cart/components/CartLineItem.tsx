236b1845606530cc18c8b0b1c7ee6c581e33dd2b
Maksim99745 <maksimmuhametzyanov11@gmail.com>
2024-06-10 17:52:06 +0300

    feat/RSS-ECOMM-4_10: correct styles

--- src/pages/Cart/components/CartLineItem.tsx
+++ src/pages/Cart/components/CartLineItem.tsx
@@ -1,5 +1,5 @@
 import { LineItem } from '@commercetools/platform-sdk';
-import { Box, Button, Grid, Paper, Typography } from '@mui/material';
+import { Box, Button, Container, Grid, Paper, Typography } from '@mui/material';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import Divider from '@mui/material/Divider';
 import { CartLineItemPrice } from './CartLineItemPrice';
@@ -14,47 +14,40 @@ export function CartLineItem({ cartItem, disabled = false, onRemove }: CartLineI
   const imagePath = cartItem?.variant?.images?.[0]?.url ?? '/public/defaultLineItemImage.jpeg';
 
   return (
-    <Grid key={cartItem.id} container>
-      <Paper elevation={2} sx={{ p: '1vh 2%', width: '100%', m: 1 }}>
-        <Grid columns={4} container spacing={1} alignItems="center">
-          <Grid item xs={1}>
-            <Box
-              component="img"
-              sx={{
-                height: 90,
-                width: 'fit-content',
-                maxWidth: { xs: 150, md: 200 },
-              }}
-              alt={cartItem.name.en}
-              src={imagePath}
-            />
+    <Container maxWidth="md">
+      <Grid key={cartItem.id} container>
+        <Paper elevation={2} sx={{ p: '1vh 2%', width: '100%', m: 1 }}>
+          <Grid columns={7} container spacing={1} alignItems="center">
+            <Grid item xs={2}>
+              <Box
+                component="img"
+                sx={{
+                  maxHeight: 100,
+                }}
+                alt={cartItem.name.en}
+                src={imagePath}
+              />
+            </Grid>
+            <Grid item xs={5}>
+              <Typography>{`${cartItem.name.en}`}</Typography>
+            </Grid>
           </Grid>
-          <Grid item xs={2}>
-            <Typography>{`${cartItem.name.en}`}</Typography>
+          <Divider sx={{ m: 1 }} />
+          <Grid key={cartItem.id} columns={7} container alignItems="centre">
+            <Grid item xs={2}>
+              <Typography>&nbsp;{`amount: ${cartItem.quantity}`}</Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <CartLineItemPrice cartItem={cartItem} />
+            </Grid>
+            <Grid item xs={2}>
+              <Button disabled={disabled} startIcon={<DeleteOutlineIcon />} onClick={() => onRemove(cartItem)}>
+                Remove{' '}
+              </Button>
+            </Grid>
           </Grid>
-        </Grid>
-        <Divider sx={{ m: 1 }} />
-        <Grid
-          key={cartItem.id}
-          columns={4}
-          spacing={{ xs: 1 }}
-          container
-          justifyContent="space-between"
-          alignItems="centre"
-        >
-          <Grid item xs={1}>
-            <Typography>&nbsp; {`amount: ${cartItem.quantity}`}</Typography>
-          </Grid>
-          <Grid item xs={2}>
-            <CartLineItemPrice cartItem={cartItem} />
-          </Grid>
-          <Grid item xs={1}>
-            <Button disabled={disabled} startIcon={<DeleteOutlineIcon />} onClick={() => onRemove(cartItem)}>
-              Remove
-            </Button>
-          </Grid>
-        </Grid>
-      </Paper>
-    </Grid>
+        </Paper>
+      </Grid>
+    </Container>
   );
 }
