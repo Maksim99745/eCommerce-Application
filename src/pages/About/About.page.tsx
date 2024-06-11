8ea1bafdad8824916921e621217b4a420f080820
tatiana <belangelphone@gmail.com>
2024-06-11 12:31:30 +0300

    feat/RSS-ECOMM-4_19: add into and team members blocks

--- src/pages/About/About.page.tsx
+++ src/pages/About/About.page.tsx
@@ -1,5 +1,29 @@
+import { Container, Stack } from '@mui/material';
+import AboutImagesContainer from './components/ImagesBlockElement/AboutImagesBlock';
+import DescriptionElement from './components/DescriptionElement/DescriptionElement';
+import TeamMembersBlock from './components/TeamMembersBlock/TeamMembersBlock';
+
 function AboutPage() {
-  return <p>About page</p>;
+  return (
+    <Stack>
+      <Container
+        component="section"
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: { lg: 'row', md: 'row', sm: 'column-reverse', xs: 'column-reverse' },
+          pt: '35px',
+          mb: { lg: '45px', sm: '25px', xs: '15px' },
+          mt: { sm: '25px', xs: '25px' },
+          gap: '4%',
+        }}
+      >
+        <DescriptionElement />
+        <AboutImagesContainer />
+      </Container>
+      <TeamMembersBlock />
+    </Stack>
+  );
 }
 
 export default AboutPage;
