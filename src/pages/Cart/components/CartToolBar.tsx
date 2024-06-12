ccbe4c1b7427b7759dbe4bcd5f86063d540783c0
Maksim99745 <maksimmuhametzyanov11@gmail.com>
2024-06-12 13:23:45 +0300

    feat/RSS-ECOMM-4_16: add promo codes

--- src/pages/Cart/components/CartToolBar.tsx
+++ src/pages/Cart/components/CartToolBar.tsx
@@ -1,6 +1,7 @@
 import { Cart } from '@commercetools/platform-sdk';
-import { Button, ButtonProps, Grid, Paper, Stack } from '@mui/material';
+import { Button, ButtonProps, Divider, Grid, Paper, Stack } from '@mui/material';
 import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
+import { useShowMessage } from '@hooks/useShowMessage';
 import { CartTotalCost } from './CartTotalCost';
 import { CleanCartDialog } from './CleanCartDialog';
 
@@ -23,6 +24,14 @@ function CleanCart(props: ButtonProps) {
 }
 
 export function CartToolBar({ cartData, isLoading, onCleanCart }: CartToolBarProps) {
+  const showMessage = useShowMessage();
+
+  const getPromoCode = (): void => {
+    const promoCodes = ['RS5', 'RS10'];
+    const promoCode = promoCodes[Math.floor(Math.random() * promoCodes.length)];
+    showMessage(`Congratulations, your promo-code is ${promoCode}`);
+  };
+
   return (
     <Grid container sx={{ position: 'sticky', top: '0', zIndex: 1, p: 0 }}>
       <Paper
@@ -37,6 +46,17 @@ export function CartToolBar({ cartData, isLoading, onCleanCart }: CartToolBarPro
           <CartTotalCost cart={cartData} />
           <CleanCartDialog openControl={CleanCart} disabled={isLoading} onCleanCart={onCleanCart} />
         </Stack>
+        <Divider sx={{ m: 1 }} />
+        <Grid container columns={12} direction="row" justifyContent="space-between" alignItems="center">
+          <Grid item xs={5}>
+            <Button size="small" onClick={() => getPromoCode()}>
+              Get promo code!
+            </Button>
+          </Grid>
+          <Grid item xs={7}>
+            s
+          </Grid>
+        </Grid>
       </Paper>
     </Grid>
   );
