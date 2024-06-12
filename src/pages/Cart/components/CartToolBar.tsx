f6a69e4c2fea9f941faafd10894ea9d014ea75ff
Maksim Mukhametzianov <137721533+Maksim99745@users.noreply.github.com>
2024-06-12 11:00:36 +0200

    RSS-ECOMM-4_18: Implement "Clear Shopping Cart" (#201)
    * feat/RSS-ECOMM-3_18: correct styles
    
    * feat/RSS-ECOMM-3_18: correct date
    
    * feat/RSS-ECOMM-3_18: correct styles
    
    * feat/RSS-ECOMM-3_18: correct styles and form reset
    
    * Feat/rss ecomm 3 18 correct styles (#178)
    
    * feat/RSS-ECOMM-3_18: correct styles
    
    * feat/RSS-ECOMM-3_18: correct date
    
    * feat/RSS-ECOMM-3_18: correct styles
    
    * feat/RSS-ECOMM-3_18: correct styles and form reset
    
    * feat/RSS-ECOMM-3_18: correct personal form
    
    * feat/RSS-ECOMM-3_18: correct personal form 2
    
    * feat/RSS-ECOMM-4_10: start task
    
    * feat/RSS-ECOMM-4_10: task progress
    
    * feat/RSS-ECOMM-4_10: product delete from card
    
    * feat/RSS-ECOMM-4_10: product drawing progress
    
    * feat/RSS-ECOMM-4_10: empty cart page
    
    * feat/RSS-ECOMM-4_10: correct styles
    
    * feat/RSS-ECOMM-4_10: correct styles 2
    
    * feat/RSS-ECOMM-4_11: add quentity changing
    
    * feat/RSS-ECOMM-4_11: add total cost
    
    * feat/RSS-ECOMM-4_11: correct style
    
    * feat/RSS-ECOMM-4_11: correct default photo
    
    * feat/RSS-ECOMM-4_11: correct whitespaces and names
    
    * feat/RSS-ECOMM-4_11: code refactor
    
    * feat/RSS-ECOMM-4_11: code refactor 2
    
    * feat/RSS-ECOMM-4_11: code refactor 3
    
    * feat/RSS-ECOMM-4_11: code refactor 4
    
    * feat/RSS-ECOMM-4_18: add remove cart feature
    
    * feat/RSS-ECOMM-4_18: correct photos 2

---
+++ src/pages/Cart/components/CartToolBar.tsx
@@ -0,0 +1,41 @@
+import { Cart } from '@commercetools/platform-sdk';
+import { Button, ButtonProps, Container, Grid, Paper, Stack } from '@mui/material';
+import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
+import { CartTotalCost } from './CartTotalCost';
+import { CleanCartDialog } from './CleanCartDialog';
+
+export type CartToolBarProps = {
+  cartData: Cart;
+  isLoading?: boolean;
+  onCleanCart: () => void;
+};
+
+function CleanCart(props: ButtonProps) {
+  return (
+    <Button variant="contained" startIcon={<RemoveShoppingCartIcon />} {...props}>
+      Clean cart
+    </Button>
+  );
+}
+
+export function CartToolBar({ cartData, isLoading, onCleanCart }: CartToolBarProps) {
+  return (
+    <Container maxWidth="md" sx={{ position: 'sticky', top: '0', zIndex: 1 }}>
+      <Grid container>
+        <Paper
+          elevation={2}
+          sx={{
+            p: '1vh 2%',
+            width: '100%',
+            m: 1,
+          }}
+        >
+          <Stack direction="row" justifyContent="space-between">
+            <CartTotalCost cart={cartData} />
+            <CleanCartDialog openControl={CleanCart} disabled={isLoading} onCleanCart={onCleanCart} />
+          </Stack>
+        </Paper>
+      </Grid>
+    </Container>
+  );
+}
--- src/pages/Cart/components/CartTotalCost.tsx
+++ src/pages/Cart/components/CartTotalCost.tsx
@@ -1,25 +1,12 @@
 import { Cart } from '@commercetools/platform-sdk';
 import { priceAmount, productCurrencyMap } from '@constants/products.const';
-import { Container, Grid, Paper, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 
 export function CartTotalCost({ cart }: { cart: Cart }) {
   const currencyCode = productCurrencyMap[cart.lineItems[0].price.value.currencyCode];
   return (
-    <Container maxWidth="md" sx={{ position: 'sticky', top: '0', zIndex: 1 }}>
-      <Grid container>
-        <Paper
-          elevation={2}
-          sx={{
-            p: '1vh 2%',
-            width: '100%',
-            m: 1,
-          }}
-        >
-          <Typography variant="h6">
-            Total price: {cart.totalPrice.centAmount / priceAmount} {currencyCode}
-          </Typography>
-        </Paper>
-      </Grid>
-    </Container>
+    <Typography variant="h6">
+      Total price: {cart.totalPrice.centAmount / priceAmount} {currencyCode}
+    </Typography>
   );
 }
